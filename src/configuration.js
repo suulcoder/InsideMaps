6ae174ce90881a3b4dd4d81d67d9fc70a5be0f7f
suulcoder <ef.proyectomundial@gmail.com>
2020-10-25 20:32:53 -0600

    fixing route

--- src/configuration.js
+++ src/configuration.js
@@ -1,4 +1,4 @@
 //uncomment this line if you are working locally:
 //export const URL = 'http://localhost:3030'
-export const URL = ' http://insidemaps.s3-website.us-east-2.amazonaws.com'
+export const URL = ' http://insidemaps.s3-website.us-east-2.amazonaws.com/'
 export const API_URL = 'https://inside-maps-api.herokuapp.com/api/v1/'
