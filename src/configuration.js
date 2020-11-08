32ce5a9f29cc5c476ab883a052257d663b695878
Gus Mendez <gus.mendez.99@gmail.com>
2020-11-07 23:20:43 -0600

    base url fixed

--- src/configuration.js
+++ src/configuration.js
@@ -1,4 +1,4 @@
 //uncomment this line if you are working locally:
-export const URL = 'http://localhost:3000/'
-//export const URL = ' http://insidemaps.s3-website.us-east-2.amazonaws.com/'
+//export const URL = 'http://localhost:3000/'
+export const URL = ' http://insidemaps.s3-website.us-east-2.amazonaws.com/'
 export const API_URL = 'https://inside-maps-api.herokuapp.com/api/v1/'
