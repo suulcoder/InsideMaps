5c353203ee628268331ea96691d8ede051c256d1
Diego Estrada <est.diego03@gmail.com>
2020-11-17 17:58:09 -0600

    finished with stats reducers

--- src/reducers/reducers.stats.test.js
+++ src/reducers/reducers.stats.test.js
@@ -17,4 +17,26 @@ describe('reports reducers', ()=>{
         expect(reducers.error(null, {type: types.FETCH_STATS_FAILED,
             payload: { error:404}})).equal(404)
     })
+    //data
+    it('should return initial state', ()=>{
+        expect(reducers.data(undefined, {})).equal(null)
+    })
+    it('should complete fecthing', ()=>{
+        expect(reducers.data(null, {type: types.FETCH_STATS_COMPLETED,
+            payload: { data:1}})).equal(1)
+    })
+    //isFetching
+    it('should return initial state', ()=>{
+        expect(reducers.isFetching(undefined, {})).equal(null)
+    })
+    it('should complete fecthing', ()=>{
+        expect(reducers.isFetching(null, {type: types.FETCH_STATS_STARTED})).equal(true)
+    })
+    it('should complete fecthing', ()=>{
+        expect(reducers.isFetching(null, {type: types.FETCH_STATS_COMPLETED})).equal(false)
+    })
+    it('should complete fecthing', ()=>{
+        expect(reducers.isFetching(null, {type: types.FETCH_STATS_FAILED})).equal(false)
+    })
+})
 })
