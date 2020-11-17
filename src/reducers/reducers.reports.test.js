76263e190a795ff88488dc56db48d501e3e508d5
Diego Estrada <est.diego03@gmail.com>
2020-11-16 21:02:38 -0600

    finished tests for error report reducers

--- src/reducers/reducers.reports.test.js
+++ src/reducers/reducers.reports.test.js
@@ -7,8 +7,35 @@ describe('reports reducers', ()=>{
     it('should return initial state', ()=>{
         expect(reducers.error(undefined, {})).equal(null)
     })
-    it('should handle authentication failed', ()=>{
-        expect(reducers.error(null, {type: types.AUTHENTICATION_FAILED,
+    it('error when fetch started', ()=>{
+        expect(reducers.error(undefined, {type: types.FETCH_ERROR_DATA_STARTED})).equal(null)
+    })
+    it('error when fetch completed', ()=>{
+        expect(reducers.error(undefined, {type: types.FETCH_ERROR_DATA_COMPLETED})).equal(null)
+    })
+    it('should handle fetch failed', ()=>{
+        expect(reducers.error(null, {type: types.FETCH_ERROR_DATA_FAILED,
             payload: { error:404}})).equal(404)
+    })
+    //data
+    it('should return initial state', ()=>{
+        expect(reducers.data(undefined, {})).equal(null)
+    })
+    it('should complete fecthing', ()=>{
+        expect(reducers.data(null, {type: types.FETCH_ERROR_DATA_COMPLETED,
+            payload: { data:1}})).equal(1)
+    })
+    //isFetching
+    it('should return initial state', ()=>{
+        expect(reducers.isFetching(undefined, {})).equal(null)
+    })
+    it('should complete fecthing', ()=>{
+        expect(reducers.isFetching(null, {type: types.FETCH_ERROR_DATA_STARTED})).equal(true)
+    })
+    it('should complete fecthing', ()=>{
+        expect(reducers.isFetching(null, {type: types.FETCH_ERROR_DATA_COMPLETED})).equal(false)
+    })
+    it('should complete fecthing', ()=>{
+        expect(reducers.isFetching(null, {type: types.FETCH_ERROR_DATA_FAILED})).equal(false)
     })
 })
