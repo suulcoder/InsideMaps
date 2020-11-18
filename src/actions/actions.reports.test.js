80036f0bd840b06644778d579bb92d66a839fd33
Diego Estrada <est.diego03@gmail.com>
2020-11-17 18:40:12 -0600

    ut for report actions

---
+++ src/actions/actions.reports.test.js
@@ -0,0 +1,30 @@
+import * as types from '../types/reports';
+import * as actions from './reports';
+
+
+describe('reports Actions', () => {
+    it('should start fetching error data', () => {
+        const expectedAction = {
+            type: types.FETCH_ERROR_DATA_STARTED,
+        }
+        expect(actions.startFetchingErrorData()).toEqual(expectedAction)
+      });
+      it('should fail fetching error data', () => {
+        const expectedAction = {
+          type: types.FETCH_MAP_FAILED,
+          payload: {
+            error: 1,
+          }
+        }
+        expect(actions.failFetchingErrorData(1)).toEqual(expectedAction)
+      });
+      it('should complete fetching error data', () => {
+        const expectedAction = {
+          type: types.FETCH_MAP_FAILED,
+          payload: {
+            data: "ERROR",
+          }
+        }
+        expect(actions.completeFetchingErrorData("ERROR")).toEqual(expectedAction)
+      });
+})
